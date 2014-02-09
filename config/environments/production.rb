cb071c9a2d7707e56af740cb1d70dcb08aa0e551
Jacob Brown <kardeiz@gmail.com>
2014-02-09 11:50:11 -0600

    new

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,16 @@ Sllapp::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.action_mailer.default_url_options = { :host => "162.243.100.39:3000" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               '162.243.100.39',
+    user_name:            'kcplsllapp',
+    password:             ENV['ad_password'],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
