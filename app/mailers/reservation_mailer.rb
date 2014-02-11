20ef6d9702d481c7caef665898f31a883588bb19
Jacob Brown <kardeiz@gmail.com>
2014-02-11 06:31:53 -0600

    notice email

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,17 +1,17 @@
 class ReservationMailer < ActionMailer::Base
   
-  default :from => "kardeiz@gmail.com"
+  default :from => "kcplsllapp@gmail.com"
   
   def notification(reservation, user)
     rdp_file = %Q{
-      full address:s:lendinglibrary1.arsalon.net\nprompt for credentials:i:1
+      full address:s:lendinglibrary1.arsalon.net\nprompt for credentials:i:1\ndrivestoredirect:s:*
     }.strip
     attachments['sll.rdp'] = rdp_file
     @reservation, @user = reservation, user
     subject = %Q{
       Software Lending Library Reservation - #{@reservation.start_date_long_formatted_string}
     }.strip
-    mail(:to => 'kardeiz@gmail.com', :subject => subject)  
+    mail(:to => @user.email, :subject => subject)  
   end
   
 end
